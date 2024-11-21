f2da8fce2fb3fb433f2cc9bda9025bbc19e68575
Javier Julio <jjfutbol@gmail.com>
2024-11-21 17:48:53 -0500

    Initial commit

---
+++ lib/rubocop-jackpocket.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rubocop'
+
+require_relative 'rubocop/jackpocket'
+require_relative 'rubocop/jackpocket/version'
+require_relative 'rubocop/jackpocket/inject'
+
+RuboCop::Jackpocket::Inject.defaults!
+
+require_relative 'rubocop/cop/jackpocket_cops'
---
+++ spec/rubocop/jackpocket_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Jackpocket do
+  it "has a version number" do
+    expect(RuboCop::Jackpocket::VERSION).not_to be nil
+  end
+end
